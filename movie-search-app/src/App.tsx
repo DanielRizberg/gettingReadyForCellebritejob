d7fd31d66178cda55910fd3224ef0041e68fcfba
daniel <meltsev1@gmail.com>
2021-11-29 10:20:45 +0200

    some changes

--- movie-search-app/src/App.tsx
+++ movie-search-app/src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { Search } from './components/search';
 import './App.css';
+import { movieData } from './models/movieData';
 
 function App() {
   const [data,setData]=useState([{}]);
-  const newData=(val:Array<any>)=>{setData(val);console.log(val)}
+  const newData=(val:Array<movieData>)=>{setData(val);console.log(val)}
   return (
    
     <div className="App">
--- movie-search-app/src/components/search.tsx
+++ movie-search-app/src/components/search.tsx
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { distinctUntilChanged, fromEvent, map, throttleTime } from "rxjs";
+import { Observable } from "rx";
+import {
+  catchError,
+  distinctUntilChanged,
+  fromEvent,
+  map,
+  of,
+  switchMap,
+  throttleTime,
+} from "rxjs";
+import { fromFetch } from "rxjs/fetch";
+import { movieData } from "../models/movieData";
+
 import { searchProps } from "../models/searchModel";
 
-
 export function Search(props: searchProps) {
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
 
   const textEl = React.useRef<HTMLInputElement>(null);
 
@@ -13,16 +24,46 @@ export function Search(props: searchProps) {
       .pipe(
         map((x) => (x.target as any).value as string),
         distinctUntilChanged(),
-        throttleTime(1000)
+        throttleTime(1000),
+        switchMap((val) => getData(val))
       )
       .subscribe((value) => {
-       console.log(value)
-       props.newData(['hello'])
+       
+        props.newData(value as any[]);
       });
 
     return () => sub.unsubscribe();
   }, []);
+  const getData = (val: string) => {
+    let obs$ = fromFetch(
+      `https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/${val}`,
+      {
+        method: "GET",
+        "headers": {
+          "x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
+          "x-rapidapi-key": "a5abc19a4bmsh6004678e99f8413p1a46a7jsn35b65af042e1"
+        }
+      }
+    ).pipe(
+      switchMap((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return of({
+            error: true,
+            message: `Error ${res.status} ${res.statusText}`,
+          });
+        }
+      }),
+      map((json) => json.titles as Array<movieData>),
+      catchError((err) => {
+        console.error(err);
+        return of({ error: true, message: err.message });
+      })
+    );
 
+    return obs$;
+  };
   return (
     <div className="container-fluid">
       <div className="row">
@@ -35,12 +76,9 @@ export function Search(props: searchProps) {
             name="searchValue"
             id="searchValue"
             ref={textEl}
-            onChange= {e=> 
-              {
-                setSearchValue(e.target.value);
-               
-            }
-          }
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+            }}
           ></input>
         </div>
       </div>
