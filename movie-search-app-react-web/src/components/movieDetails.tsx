92c4c3d5c1745e4baaacb5676fd0499974982edd
daniel <meltsev1@gmail.com>
2021-12-14 09:55:09 +0200

    add movue details

--- movie-search-app-react-web/src/components/movieDetails.tsx
+++ movie-search-app-react-web/src/components/movieDetails.tsx
@@ -1,25 +1,48 @@
-
 import { Button, Modal } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 import { movieDetailModel } from "../models/movieDetailModel";
 
 export function MovieDetails(props: movieDetailModel) {
-  
-
- 
-
   return (
     <>
-      <Modal show={props.show} onHide={props.handleClose} >
-        <Modal.Header closeButton={true} >
+      <Modal
+        show={props.show}
+        onHide={props.handleClose}
+        fullscreen={"lg-down"}
+        size={"xl"}
+      >
+        <Modal.Header closeButton={true}>
           <Modal.Title>Movie Details</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={props.handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
+        <Modal.Body>
+   
+          <div className="container-fluid" >
+           
+            <div className="row">
+              <div className="col">
+                <h6> plot:</h6>
+                <p>{props.data.plot}</p>
+              </div>
+              <div className="col">
+                <h6> cast:</h6>
+                <p>
+                  {props.data.cast
+                    .map((x) => `${x.actor}  as :${x.character} .`)
+                    .join(" \n")}
+                </p>
+              </div>
+            </div>
+            <div className="row mb-4" >
+              <div className="col">
+                <h5>trailer link</h5>
+                <a href={props.data.trailer.link} target="_blank" rel="noreferrer" >{props.data.trailer.link}</a>
+              </div>
+              
+            </div>
+          </div>
+          <img className="img-thumbnail img-fluid text-center" src={props.data.poster} alt="" style={{margin:'auto',height:'50vh'}} />
+        </Modal.Body>
+      
       </Modal>
     </>
   );
